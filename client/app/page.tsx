dc174a70d1bfa4a6d1503806fe2303515dd00921
Ariela Israel <arielaisrael12@gmail.com>
2024-06-07 08:25:19 -0400

    +updated dashboard/home

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import ReservationsTable from './(components)/dashboard/ReservationsList';
+
+export default async function Home() {
+	const getSubscriptions = async () => {
+		try {
+			const res = await fetch(`http://localhost:5000/reservations`, {
+				cache: 'no-cache',
+			});
+			return res.json();
+		} catch (err: any) {
+			console.error(`Failed to fetch reservations: ${err.message}`);
+		}
+	};
+
+	const reservations = await getSubscriptions();
+
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<h1 className='text-5xl font-bold'>Periodic Tables</h1>
+		<main className='ml-64 px-24 py-16'>
+			<h1 className='text-5xl mb-4'>Reservations</h1>
+			<ReservationsTable reservations={reservations} />
 		</main>
 	);
 }

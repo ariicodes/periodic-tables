52fb60ba7cc4097298e8753d8de81defeeb16966
Ariela Israel <arielaisrael12@gmail.com>
2024-06-07 06:54:24 -0400

    * reservations fix

--- server/src/models/Reservation.ts
+++ server/src/models/Reservation.ts
@@ -5,10 +5,9 @@ interface Reservation {
 	last_name: string;
 	mobile_number: string;
 	reservation_date: Date;
-	reservation_time: Date;
 	party_size: number;
-	createdAt: number;
-	updatedAt: number;
+	createdAt: string;
+	updatedAt: string;
 }
 
 const reservationSchema = new mongoose.Schema<Reservation>(
@@ -17,10 +16,9 @@ const reservationSchema = new mongoose.Schema<Reservation>(
 		last_name: { type: String, required: true },
 		mobile_number: { type: String, required: true },
 		reservation_date: { type: Date, required: true },
-		reservation_time: { type: Date, required: true },
 		party_size: { type: Number, required: true },
-		createdAt: { type: Number, required: true },
-		updatedAt: { type: Number, required: true },
+		createdAt: { type: String, required: true },
+		updatedAt: { type: String, required: true },
 	},
 	{
 		// Make Mongoose use Unix time (seconds since Jan 1, 1970)
